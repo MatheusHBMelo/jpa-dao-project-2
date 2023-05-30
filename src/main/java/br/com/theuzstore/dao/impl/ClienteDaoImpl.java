c579237aea81a15f6266ccf3700867f6d5f6cdb0
MatheusHBMelo <matheushbmelo@gmail.com>
2023-05-30 10:05:02 -0300

    [FEAT] Added the implementation class of ClientDao interface

---
+++ src/main/java/br/com/theuzstore/dao/impl/ClienteDaoImpl.java
@@ -0,0 +1,33 @@
+package br.com.theuzstore.dao.impl;
+
+import br.com.theuzstore.dao.ClienteDao;
+import br.com.theuzstore.model.entities.Cliente;
+
+import java.util.List;
+
+public class ClienteDaoImpl implements ClienteDao {
+    @Override
+    public void insert(Cliente cliente) {
+
+    }
+
+    @Override
+    public void update(Cliente cliente) {
+
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+
+    @Override
+    public Cliente findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Cliente> findAll() {
+        return null;
+    }
+}
