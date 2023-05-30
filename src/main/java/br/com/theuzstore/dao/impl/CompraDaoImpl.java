a650471cb61959dd25c6330ecbd055a0158a6c6b
MatheusHBMelo <matheushbmelo@gmail.com>
2023-05-30 10:05:19 -0300

    [FEAT] Added the implementation class of CompraDao interface

---
+++ src/main/java/br/com/theuzstore/dao/impl/CompraDaoImpl.java
@@ -0,0 +1,39 @@
+package br.com.theuzstore.dao.impl;
+
+import br.com.theuzstore.dao.CompraDao;
+import br.com.theuzstore.model.entities.Cliente;
+import br.com.theuzstore.model.entities.Compra;
+
+import java.util.List;
+
+public class CompraDaoImpl implements CompraDao {
+    @Override
+    public void insert(Compra compra) {
+
+    }
+
+    @Override
+    public void update(Compra compra) {
+
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+
+    @Override
+    public Compra findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Compra> findByCliente(Cliente cliente) {
+        return null;
+    }
+
+    @Override
+    public List<Compra> findAll() {
+        return null;
+    }
+}
