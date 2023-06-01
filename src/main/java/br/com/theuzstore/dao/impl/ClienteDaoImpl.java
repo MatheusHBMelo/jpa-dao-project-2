31069d255f9650cea9f1ec80820ddf2c0236a8ac
MatheusHBMelo <matheushbmelo@gmail.com>
2023-06-01 10:27:17 -0300

    [FEAT] Implementation of Daos

--- src/main/java/br/com/theuzstore/dao/impl/ClienteDaoImpl.java
+++ src/main/java/br/com/theuzstore/dao/impl/ClienteDaoImpl.java
@@ -1,38 +1,131 @@
 package br.com.theuzstore.dao.impl;
 
 import br.com.theuzstore.dao.ClienteDao;
+import br.com.theuzstore.exceptions.ClienteNotFoundException;
+import br.com.theuzstore.exceptions.ForeignKeyConstraintException;
+import br.com.theuzstore.exceptions.NullArgumentException;
 import br.com.theuzstore.model.entities.Cliente;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
 import java.util.List;
 
 public class ClienteDaoImpl implements ClienteDao {
-    @PersistenceContext
     private EntityManager entityManager;
+
+    public ClienteDaoImpl() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-test");
+        entityManager = emf.createEntityManager();
+    }
 
     @Override
     public void insert(Cliente cliente) {
+        if (cliente == null) {
+            throw new NullArgumentException("[Erro ao inserir] O objeto cliente não pode ser nulo.");
+        }
 
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(cliente);
+            entityManager.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void update(Cliente cliente) {
+        if (cliente == null) {
+            throw new NullArgumentException("[Erro ao atualizar] O Objeto cliente não pode ser nulo.");
+        }
+        try {
+            Cliente clientePersistido = entityManager.find(Cliente.class, cliente.getId());
+
+            if (clientePersistido == null) {
+                throw new ClienteNotFoundException("[Erro ao atualizar] O cliente especificado não existe no banco de dados.");
+            }
+
+            if (cliente.getNome() == null) {
+                cliente.setNome(clientePersistido.getNome());
+            }
+
+            if (cliente.getLogin() == null) {
+                cliente.setLogin(clientePersistido.getLogin());
+            }
+
+            if (cliente.getEmail() == null) {
+                cliente.setEmail(clientePersistido.getEmail());
+            }
+
+            if (cliente.getSenha() == null) {
+                cliente.setSenha(clientePersistido.getSenha());
+            }
+
+            entityManager.getTransaction().begin();
+            entityManager.merge(cliente);
+            entityManager.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            entityManager.close();
+        }
 
     }
 
     @Override
     public void delete(Integer id) {
+        if (id == null) {
+            throw new NullArgumentException("[Erro ao excluir] O Id especificado não pode ser nulo.");
+        }
 
+        try {
+            Cliente clientePersistido = entityManager.find(Cliente.class, id);
+            if (clientePersistido == null) {
+                throw new ClienteNotFoundException("[Erro ao excluir] O cliente especificado não existe no banco de dados.");
+            }
+
+            entityManager.getTransaction().begin();
+            entityManager.remove(clientePersistido);
+            entityManager.getTransaction().commit();
+        } catch (ForeignKeyConstraintException fk) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            throw fk;
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public Cliente findById(Integer id) {
-        return null;
+        if (id == null) {
+            throw new NullArgumentException("[Erro ao buscar] O Id não pode ser nulo.");
+        }
+
+        Cliente clientePersistido = entityManager.find(Cliente.class, id);
+        if (clientePersistido == null) {
+            throw new ClienteNotFoundException("[Erro ao buscar] O cliente buscado não existe no banco de dados.");
+        }
+
+        return clientePersistido;
     }
 
     @Override
     public List<Cliente> findAll() {
-        return null;
+        TypedQuery<Cliente> query = entityManager.createQuery("SELECT c FROM Cliente c", Cliente.class);
+        List<Cliente> clientes = query.getResultList();
+        if (clientes.isEmpty()) {
+            throw new NoResultException("[Erro ao buscar] Não existem clientes registrados.");
+        }
+
+        return clientes;
     }
 }
--- src/main/java/br/com/theuzstore/dao/impl/CompraDaoImpl.java
+++ src/main/java/br/com/theuzstore/dao/impl/CompraDaoImpl.java
@@ -1,44 +1,150 @@
 package br.com.theuzstore.dao.impl;
 
 import br.com.theuzstore.dao.CompraDao;
+import br.com.theuzstore.exceptions.ClienteNotFoundException;
+import br.com.theuzstore.exceptions.CompraNotFoundException;
+import br.com.theuzstore.exceptions.ForeignKeyConstraintException;
+import br.com.theuzstore.exceptions.NullArgumentException;
 import br.com.theuzstore.model.entities.Cliente;
 import br.com.theuzstore.model.entities.Compra;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
 import java.util.List;
 
 public class CompraDaoImpl implements CompraDao {
-    @PersistenceContext
     private EntityManager entityManager;
+
+    public CompraDaoImpl() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-test");
+        entityManager = emf.createEntityManager();
+    }
 
     @Override
     public void insert(Compra compra) {
+        if (compra == null) {
+            throw new NullArgumentException("[Erro ao inserir] O objeto compra não pode ser nulo.");
+        }
 
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(compra);
+            entityManager.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void update(Compra compra) {
+        if (compra == null) {
+            throw new NullArgumentException("[Erro ao atualizar] O Objeto cliente não pode ser nulo.");
+        }
 
+        try {
+            Compra compraPersistida = entityManager.find(Compra.class, compra.getId());
+
+            if (compraPersistida == null) {
+                throw new CompraNotFoundException("[Erro ao atualizar] A compra especificada não existe no banco de dados.");
+            }
+
+            if (compra.getValor() == null) {
+                compra.setValor(compraPersistida.getValor());
+            }
+
+            if (compra.getData() == null) {
+                compra.setData(compraPersistida.getData());
+            }
+
+            if (compra.getObservacao() == null) {
+                compra.setObservacao(compraPersistida.getObservacao());
+            }
+
+            if (compra.getCliente() == null) {
+                compra.setCliente(compraPersistida.getCliente());
+            }
+
+            entityManager.getTransaction().begin();
+            entityManager.merge(compra);
+            entityManager.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void delete(Integer id) {
+        if (id == null) {
+            throw new NullArgumentException("[Erro ao excluir] O Id especificado não pode ser nulo.");
+        }
 
+        try {
+            Compra compraPersistida = entityManager.find(Compra.class, id);
+
+            if (compraPersistida == null) {
+                throw new CompraNotFoundException("[Erro ao excluir] A compra especificada não existe no banco de dados.");
+            }
+
+            entityManager.getTransaction().begin();
+            entityManager.remove(compraPersistida);
+            entityManager.getTransaction().commit();
+        } catch (ForeignKeyConstraintException fk) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            throw fk;
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public Compra findById(Integer id) {
-        return null;
+        if (id == null) {
+            throw new NullArgumentException("[Erro ao buscar] O Id não pode ser nulo.");
+        }
+
+        Compra compraPersistida = entityManager.find(Compra.class, id);
+        if (compraPersistida == null) {
+            throw new CompraNotFoundException("[Erro ao buscar] A compra buscada não existe no banco de dados.");
+        }
+
+        return compraPersistida;
     }
 
     @Override
     public List<Compra> findByCliente(Cliente cliente) {
-        return null;
+        if (cliente == null) {
+            throw new NullArgumentException("[Erro ao buscar] O Objeto cliente não pode ser nulo.");
+        }
+
+        TypedQuery<Compra> query = entityManager.createQuery("SELECT c FROM Compra c WHERE cliente_id_cliente = :id", Compra.class);
+        query.setParameter("id", cliente.getId());
+        List<Compra> compras = query.getResultList();
+        if (compras.isEmpty()){
+            throw new NoResultException("[Erro ao buscar] Não existem compras registradas.");
+        }
+
+        return compras;
     }
 
     @Override
     public List<Compra> findAll() {
-        return null;
+        TypedQuery<Compra> query = entityManager.createQuery("SELECT c FROM Compra c", Compra.class);
+        List<Compra> compras = query.getResultList();
+        if (compras.isEmpty()) {
+            throw new NoResultException("[Erro ao buscar] Não existem compras registradas.");
+        }
+
+        return compras;
     }
 }
